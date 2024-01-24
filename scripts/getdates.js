a3db2ee90b61730e4d226bf2949252502300393f
anthonytrillo <anthony.trillo.92@gmail.com>
2024-01-24 12:14:53 -0300

    fix date format

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,6 +4,9 @@ const currentYear = new Date().getFullYear();
 const date = document.querySelector('#date');
 const lastModified = document.querySelector('#lastModified');
 let lastModif = new Date(document.lastModified);
+let dateOnly = lastModif.toLocaleDateString();
+let timeOnly = lastModif.toLocaleTimeString();
+
 
 date.textContent = `${copyrightSymbol} ${currentYear}`;
-lastModified.textContent = `${lastModifText} ${lastModif}`
+lastModified.textContent = `${lastModifText} ${dateOnly} ${timeOnly}`;
